ce3e3632ec1a996ca623717b4a34adfd928f063b
rdelva <roseldelva@gmail.com>
2021-09-12 01:37:52 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 const url = 'https://randomuser.me/api/';
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
+
+// 1. Set up connection to the Api
+// 2. find out out where most of the data is comming From
+// 3. use fetch 
+
