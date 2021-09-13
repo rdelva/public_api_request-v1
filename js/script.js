5fdef31ec3718154d97e1a9b9b3206a30ce44c23
rdelva <roseldelva@gmail.com>
2021-09-12 22:40:53 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -5,4 +5,17 @@ const gallery = document.getElementById('gallery');
 // 1. Set up connection to the Api
 // 2. find out out where most of the data is comming From
 // 3. use fetch 
+//4. Grab data for name, 
+//4 build the boxes first
+
+//=============================
+//Fetch Functions
+//==============================
+
+
+//=============================
+//Helper Functions
+//==============================
+
+/*Generate Cards*/
 
