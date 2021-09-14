c5bbb7e41c2e52ea53227382fa1047f91c5b2c94
rdelva <roseldelva@gmail.com>
2021-09-13 21:48:40 -0400

    setting up template for generateCard()

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const url = 'https://randomuser.me/api/';
 const searchContainer = document.querySelector('.search-container');
-const gallery = document.getElementById('gallery');
 
 // 1. Set up connection to the Api
 // 2. find out out where most of the data is comming From
@@ -19,3 +18,32 @@ const gallery = document.getElementById('gallery');
 
 /*Generate Cards*/
 
+function generateCard (){
+    const gallery = document.getElementById('gallery');
+    let html = '';
+
+    for(let i = 0; i < 50; i++){
+
+        html += `
+        <a href="">
+            <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">first last</h3>
+                <p class="card-text">email</p>
+                <p class="card-text cap">city, state</p>
+            </div>
+            </div>
+         </a>   
+        `;
+
+
+    }// end of for loop
+
+    gallery.insertAdjacentHTML('beforeend', html);
+}
+
+
+generateCard();
