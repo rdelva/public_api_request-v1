ea2b6af781cddee5e8a882c15fbbf47a05a1f9e4
rdelva <roseldelva@gmail.com>
2021-09-15 19:24:01 -0400

    working on generateCard and generateModal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const url = 'https://randomuser.me/api/';
 const searchContainer = document.querySelector('.search-container');
 
+
 // 1. Set up connection to the Api
 // 2. find out out where most of the data is comming From
 // 3. use fetch 
@@ -18,14 +19,16 @@ const searchContainer = document.querySelector('.search-container');
 
 /*Generate Cards*/
 
-function generateCard (){
+function generateCard(){
+    
     const gallery = document.getElementById('gallery');
+   
     let html = '';
 
     for(let i = 0; i < 50; i++){
 
         html += `
-        <a href="">
+        
             <div class="card">
             <div class="card-img-container">
                 <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
@@ -36,14 +39,99 @@ function generateCard (){
                 <p class="card-text cap">city, state</p>
             </div>
             </div>
-         </a>   
+            
         `;
 
 
     }// end of for loop
 
     gallery.insertAdjacentHTML('beforeend', html);
+
+    const cards = document.querySelectorAll('.card');
+    //console.log(cards);
+    selectCards(cards);
 }
 
 
+
+function generateModal() {
+    let html = '';
+    html += `     
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                <h3 id="name" class="modal-name cap">name</h3>
+                <p class="modal-text">email</p>
+                <p class="modal-text cap">city</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555</p>
+                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>
+
+        
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>`;
+
+    gallery.insertAdjacentHTML('beforeend', html);
+        const modal = document.querySelector('.modal-container');
+   
+    modal.style.display = 'none';
+
+
+}
+
+function selectCards(cards){
+    //console.log(cards);
+   
+    window.addEventListener('click', (e) => {
+        //console.log(e.target.classList);
+        if(e.target.classList.contains('card') ){
+            generateModal();
+            const modal = document.querySelector('.modal-container');
+            modal.style.display = 'block';
+            modalWindow();
+        }
+ 
+    });
+ 
+
+}
+
+
+function modalWindow() {
+    
+    const close = document.getElementById('modal-close-btn');
+    console.log(close);
+
+    console.log('hi');
+    close.addEventListener('click', (e) => {
+        //e.preventDefault();
+        console.log(e);
+        //console.log('hi');
+        // if(e.target.tagName == 'BUTTON'){
+        //     modal.style.display = 'none';
+        //     console.log("hi");
+
+        // }
+
+    });
+}
+
+
+
+
+
+
 generateCard();
+generateModal();
+selectCards();
+
+
+
