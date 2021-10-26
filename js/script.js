fdec5c31ec5089097aebc9b91be53e583e485bd7
rdelva <roseldelva@gmail.com>
2021-10-26 19:57:14 -0400

    created fixDate() helper method which changes dates to requested format

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ const searchContainer = document.querySelector('.search-container');
 //4. Grab data for name, 
 //4 build the boxes first
 
+
+
+ /**
+    * Use fetch data to pull json data into app
+    * @param (object with arrays) url - contains random user data
+    */
 //=============================
 //Fetch Functions
 //==============================
@@ -18,9 +24,13 @@ function fetchData(url){
 }
 
 
+ /**
+    * Invokes fetchData sends the url to the function and if it passes sends data to generateCard
+    * @param (string) url - location of where the api is located
+    */
 fetchData('https://randomuser.me/api/?results=12&nat=us')    
     .then(data => generateCard(data.results))
-    
+      
     
 
 
@@ -39,9 +49,13 @@ function checkStatus(response){
 
 
 
-/*Generate Cards*/
-
+/**
+* generateCard()
+* Invokes fetchData sends the url to the function and if it passes sends data to generateCard
+* @param (array of objects) users - list of all the users
+*/
 function generateCard(users){
+    console.log(users);
     
     const gallery = document.getElementById('gallery');   
     let html = '';
@@ -67,9 +81,10 @@ function generateCard(users){
 
 
 function selectCard(users){
+   
     const gallery = document.querySelector('#gallery');
     const cards = gallery.getElementsByClassName('card');
-    console.log(cards);
+    
     gallery.addEventListener('click', (e) => { 
 
         if(e.target.classList.contains('card')){                    
@@ -83,6 +98,8 @@ function selectCard(users){
 
 
 }
+
+
 
 function generateModal(name, users ) {
  
@@ -102,16 +119,18 @@ function generateModal(name, users ) {
                 <hr>
                 <p class="modal-text">${selectedUser[0].phone}</p>
                 <p class="modal-text">${selectedUser[0].location.street.number} ${selectedUser[0].location.street.name} ${selectedUser[0].location.city}, ${selectedUser[0].location.state} ${selectedUser[0].location.postcode}</p>
-                <p class="modal-text">Birthday: ${selectedUser[0].dob.date} 10/21/2015</p>
+                <p class="modal-text">Birthday: ${fixDate(selectedUser[0].dob.date)}</p>
             </div>
         </div>
-
+        
         
         <div class="modal-btn-container">
             <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
             <button type="button" id="modal-next" class="modal-next btn">Next</button>
         </div>
     </div>`;
+
+    fixDate(selectedUser[0].dob.date);
 
     gallery.insertAdjacentHTML('afterend', html);
     const close = document.getElementById('modal-close-btn');
@@ -121,18 +140,34 @@ function generateModal(name, users ) {
    
     close.addEventListener('click', (e) => {
         const modal = document.querySelector(".modal-container");
-        console.log(modal);
+        //console.log(modal);
         e.preventDefault();       
 
         //removes the modal from the code in order for it be reinserted again
-        modal.remove();
-        
-
-
+        modal.remove();      
     });  
-
 }
 
+/**
+* Changes date listed in array to local time format and sets to a MM-DD-YYYY
+* @param (string) fullDate - this date comes from the generateModal()
+*/
+function fixDate(fullDate){  
+   
+    const d = new Date(fullDate).toLocaleDateString();
+    const seperatedDate = d.split('/');
+    console.log(seperatedDate);
+    //Inserts O for a single digit value
+    for(let i=0; i < seperatedDate.length; i++){  //will only look at the month and date
+        if(seperatedDate[i] < 10) {
+            seperatedDate[i] = `0${seperatedDate[i]}`;
+        }
+    }
+    const birthDate = seperatedDate.join('-');    
+    console.log(birthDate);
+
+    return birthDate;
+}
 
 
 
